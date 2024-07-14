3b3d0c55097af9056ba56d17434f6511d6cf5c69
Shantanu Surjekar <shantanusurjekar104@gmail.com>
2024-07-14 18:11:46 +0530

    All Files move to GitHub

---
+++ READ.md
@@ -0,0 +1,6 @@
+# Java Programme
+
+- Core Java
+- Collection Frame work
+- OPPOSE Concept
+- String Method
---
+++ src/ex_07072024/LAB001.java
@@ -0,0 +1,7 @@
+package ex_07072024;
+
+public class LAB001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
