ee5d29609a3906f68d5e12193edf06841d7b1fe8
Shantanu Surjekar <shantanusurjekar104@gmail.com>
2024-08-03 13:48:26 +0530

    New File with Content of 20 July

---
+++ src/ex_20072024Conditionsandloops/Lab018Scanner.java
@@ -0,0 +1,17 @@
+package ex_20072024Conditionsandloops;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Lab018Scanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+        if(age > 18) {
+            System.out.println("Goa");
+        } else if (age < 18) {
+            System.out.println("School");
+        }
+    }
+}
---
+++ src/ex_20072024Conditionsandloops/Lab021Switch.java
@@ -0,0 +1,53 @@
+package ex_20072024Conditionsandloops;
+
+import java.util.Scanner;
+
+public class Lab021Switch {
+    public static void main(String[] args) {
+  // Take user input as char and tell the user if it is a vowel
+  // aeiou
+
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter a char, I will tell you, If it is a vowel or not!");
+        char user_input = sc.next().charAt(0);
+        System.out.println(user_input);
+
+        switch (user_input) {
+            case 'a' :
+                System.out.println("vowel");
+                break;
+            case 'e' :
+                System.out.println("vowel");
+                break;
+            case 'i' :
+                System.out.println("vowel");
+                break;
+            case 'o' :
+                System.out.println("vowel");
+                break;
+            case 'u' :
+                System.out.println("vowel");
+                break;
+            default:
+                System.out.println("Not a vowel, it is Consonant");
+        }
+
+        switch (user_input) {
+            case 'a', 'e', 'i', 'o', 'u':
+                System.out.println("vowel");
+                break;
+                default:
+                    System.out.println("Not a vowel,is Consonant");
+        }
+
+        // Same function by using If Else
+
+        char ch = 'a';
+        if ( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+            System.out.println("Vowel");
+        }else{
+            System.out.println("Consonet");
+        }
+
+    }
+}
