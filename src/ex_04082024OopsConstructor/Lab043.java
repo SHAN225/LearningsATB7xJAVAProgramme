7c72cc4ec1d7090b4f8821670603f88e317e15d4
SHAN225 <shantanusurjekar104@gmail.com>
2024-10-19 11:51:45 +0530

    Delete src/ex_04082024OopsConstructor directory

--- src/ex_04082024OopsConstructor/Lab043.java
+++
@@ -1,9 +0,0 @@
-package ex_04082024OopsConstructor;
-
-public class Lab043 {
-    public static void main(String[] args) {
-        car c1 = new car ();
-
-        //This is Default Constructor
-    }
-}
